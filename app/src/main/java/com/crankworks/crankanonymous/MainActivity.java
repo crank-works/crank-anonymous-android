311ac7a193735f2813dd0e49d503212557eab410
Marcus Rugger <marcusrugger@yahoo.com>
2014-12-01 06:27:48 -0500

    Add primitive action bar Updated sdk target to 21 Minor clean up

--- app/src/main/java/com/crankworks/crankanonymous/MainActivity.java
+++ app/src/main/java/com/crankworks/crankanonymous/MainActivity.java
@@ -2,8 +2,14 @@ package com.crankworks.crankanonymous;
 
 import android.app.Activity;
 import android.os.Bundle;
+//import android.support.v7.app.ActionBarActivity;
+
+// For action bar
+import android.view.Menu;
+import android.view.MenuInflater;
 
 // Launch Separate Activity
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 
@@ -15,9 +21,37 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_track:
+                openTracker();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onTrackClicked(View view) {
+        openTracker();
+    }
+
+    private void openTracker() {
         Intent intent = new Intent(this, TrackingActivity.class);
         startActivity(intent);
     }
