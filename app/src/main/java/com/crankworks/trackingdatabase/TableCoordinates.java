d946efb643bf1d4d1eafa403a99e3b05f99c2ef0
Marcus Rugger <marcusrugger@yahoo.com>
2014-12-12 08:19:22 -0500

    Add column names to tables

--- app/src/main/java/com/crankworks/trackingdatabase/TableCoordinates.java
+++ app/src/main/java/com/crankworks/trackingdatabase/TableCoordinates.java
@@ -11,6 +11,18 @@ public class TableCoordinates
 
     static final String TABLE_NAME = "coordinates";
 
+    public static final String FIELD_ID             = "_id";
+    public static final String FIELD_TRIP_ID        = "trip_id";
+    public static final String FIELD_ACCURACY       = "accuracy";
+    public static final String FIELD_ALTITUDE       = "altitude";
+    public static final String FIELD_BEARING        = "bearing";
+    public static final String FIELD_ELAPSED_TIME   = "elapsed_time";
+    public static final String FIELD_LATITUDE       = "latitude";
+    public static final String FIELD_LONGITUDE      = "longitude";
+    public static final String FIELD_PROVIDER       = "provider";
+    public static final String FIELD_SPEED          = "speed";
+    public static final String FIELD_TIME           = "time";
+
     private Database mDb;
 
     static void createTable(SQLiteDatabase db)
--- app/src/main/java/com/crankworks/trackingdatabase/TableTrips.java
+++ app/src/main/java/com/crankworks/trackingdatabase/TableTrips.java
@@ -11,6 +11,16 @@ public class TableTrips
 
     static final String TABLE_NAME = "trips";
 
+    public static final String FIELD_ID             = "_id";
+    public static final String FIELD_START_TIME     = "start_time";
+    public static final String FIELD_END_TIME       = "end_time";
+    public static final String FIELD_OBJECTIVE      = "objective";
+    public static final String FIELD_LATITUDE_HIGH  = "latitude_high";
+    public static final String FIELD_LATITUDE_LOW   = "latitude_low";
+    public static final String FIELD_LONGITUDE_HIGH = "longitude_high";
+    public static final String FIELD_LONGITUDE_LOW  = "longitude_low";
+    public static final String FIELD_DISTANCE       = "distance";
+
     private Database mDb;
 
     static void createTable(SQLiteDatabase db)
