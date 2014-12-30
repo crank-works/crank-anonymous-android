f1c841263b7a0d86d9dc0e3200fb1652ad306b46
Marcus Rugger <marcusrugger@yahoo.com>
2014-12-30 00:56:27 -0500

    Get unit tests running on Travis

--- app/src/androidTest/java/com/crankworks/crankanonymous/DisplayUnitsEnglishTest.java
+++ app/src/androidTest/java/com/crankworks/crankanonymous/DisplayUnitsEnglishTest.java
@@ -27,25 +27,29 @@ public class DisplayUnitsEnglishTest extends TestCase
 
     public void testFormatSpeed() throws Exception
     {
+        assertEquals("0.0 MPH", displayUnits.formatSpeed(0.0));
         assertEquals("10.0 MPH", displayUnits.formatSpeed(4.4704));
         assertEquals("22.4 MPH", displayUnits.formatSpeed(10.0));
     }
 
     public void testFormatAltitude() throws Exception
     {
+        assertEquals("0.0 feet", displayUnits.formatAltitude(0.0));
         assertEquals("1000.0 feet", displayUnits.formatAltitude(304.8));
         assertEquals("3280.8 feet", displayUnits.formatAltitude(1000.0));
     }
 
     public void testFormatDistance() throws Exception
     {
+        assertEquals("0.0 miles", displayUnits.formatDistance(0.0));
         assertEquals("1.0 miles", displayUnits.formatDistance(1609.3));
         assertEquals("0.6 miles", displayUnits.formatDistance(1000.0));
     }
 
     public void testFormatAccuracy() throws Exception
     {
-        assertEquals("1000.0 feet", displayUnits.formatAltitude(304.8));
-        assertEquals("3280.8 feet", displayUnits.formatAltitude(1000.0));
+        assertEquals("0.0 feet", displayUnits.formatAccuracy(0.0));
+        assertEquals("1000.0 feet", displayUnits.formatAccuracy(304.8));
+        assertEquals("3280.8 feet", displayUnits.formatAccuracy(1000.0));
     }
 }
